f081f8ac61114c686592445c2f591540fa130e89
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-24 09:05:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pandas_datareader.data import DataReader
 from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 from pypfopt import EfficientFrontier
 from pypfopt import risk_models
