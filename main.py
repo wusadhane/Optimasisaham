f998ac9eb71ba641333017a4c046fd2e3bf34823
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-24 10:10:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from pandas_datareader import data as pdr
-import matplotlib.pyplot as plt
 
 st.title("Optimasi Portofolio Saham")
 
