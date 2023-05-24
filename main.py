57bfc3ff76322dc953ccf3164bc7ca4d3333455a
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-24 14:59:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import numpy as np
 import pandas as pd
 
 st.title("Optimasi Portofolio Saham")
-
 st.markdown(
     "Index Harga Saham Gabungan Sektor Transportasi Laut")
+
 st.number_input("Masukan Dana Anda", key="dana")
+
+start = st.date_input(
+    "When\'s your birthday",
+    datetime.date(2022, 1, 1))
+st.write('Tanggal mulai:', start)
+
+end = st.date_input(
+    "When\'s your birthday",
+    datetime.now())
+st.write('Tanggal akhir:', end)
