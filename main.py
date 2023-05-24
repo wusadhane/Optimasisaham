80fc5eff97d3f56ad45ca72ae6b85a7391dd2300
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-24 15:21:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ st.markdown(
 
 st.number_input("Masukan Dana Anda", key="dana")
 
-d = st.date_input(
+s = st.date_input(
     "Tanggal Awal",
     datetime.date(2022, 1, 1), key="start")
 st.write('Tanggal Awal:', d)
 
-d = st.date_input(
+e = st.date_input(
     "Tanggal Akhir",
     datetime.date(2023, 1, 1), key="end")
 st.write('Tanggal Akhir:', d)
