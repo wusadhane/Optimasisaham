e496e50044ac1931545d5c654d906205ffae757c
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-28 15:51:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ st.number_input("Masukan Dana Anda", key="dana")
 d = st.date_input(
     "Tanggal Awal Optimasi",
     datetime.date(2022, 1, 1), key="start")
-st.write('Tanggal Awal:', d)
+
+
+namasaham = st.selectbox(
+    'Pilih saham',
+    ('BBRM.JK','BESS.JK','BSML.JK','BULL.JK', 'CANI.JK', 'HAIS.JK','HITS.JK','IPCC.JK', 'MITI.JK', 'NELY.JK', "PSSI.JK", "RIGS.JK", "SHIP.JK", "SMDR.JK", "SOCI.JK", "TCPI.JK", "TMAS.JK", "TPMA.JK", "WINS.JK")
+
 
 
 st.button(label="Proses", key="Proses",on_click=None)
