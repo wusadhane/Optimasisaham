3f7e3ad6b03bd57a8df3d6d92a82cfe253d66401
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-29 19:16:14 +0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -2,3 +2,25 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import streamlit as st
+
+# mendapatkan data dari web trading view bs4
+def getAllContent():
+    url = (
+        "https://id.tradingview.com/markets/stocks-indonesia/sectorandindustry-sector/"
+    )
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    return [
+        target["href"].split("/")[4].replace("-", " ")
+        for target in soup.select(".tv-screener__symbol")
+    ]
+  
+def spesificContent(target):
+    url = (
+        "https://www.tradingview.com/markets/stocks-indonesia/sectorandindustry-industry/marine-shipping/"
+        + target.replace(" ", "-")
+    )
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    return [target.text for target in soup.select(".tv-screener__symbol")]
+
