005c11e2d6467b89244fcd1ff06a16c545368348
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-29 09:56:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import datetime
-import yfinance as yf
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
-#datetime
-start = datetime.now()
-end = start - relativedelta(years=1)
-
-#get data saham
-saham = ['BBRM.JK','BESS.JK','BSML.JK','BULL.JK', 'CANI.JK', 'HAIS.JK','HITS.JK','IPCC.JK', 'MITI.JK', 'NELY.JK', "PSSI.JK", "RIGS.JK", "SHIP.JK", "SMDR.JK", "SOCI.JK", "TCPI.JK", "TMAS.JK", "TPMA.JK", "WINS.JK" ]
-get_saham = yf.download(saham, start=end, end=start)
-harga = get_saham["Close"].dropna(how="all")
 
 st.title("Optimasi Portofolio Saham")
 st.markdown(
