3f619554b569439638f4403888cb1c87d7a25984
wusadhane <83650015+wusadhane@users.noreply.github.com>
2023-05-30 13:25:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,17 +15,7 @@ d = st.date_input(
     datetime.date(2022, 1, 1), key="start")
 
 
-dropdown = st.selectbox(
-    "Pilih Saham", key='pilih_saham')
-
-if dropdown:
-    start_predict = st.date_input(
-        "Tanggal Awal Optimasi", value=pd.to_datetime("2023-01-01"), min_value=pd.to_datetime("2023-01-01"), key='input_start')
-
-    end_predict = st.date_input("Tanggal Akhir Optimasi",
-                                value=pd.to_datetime("today"), key='input_end')
-
-periode = (start_predict - end_predict).days - 1
+dropdown = st.selectbox("Pilih Saham", key='pilih_saham')
 
 
 st.button(label="Proses", key="Proses",on_click=None)
